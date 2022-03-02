b91165c13a83fc27f8e790f3a45f6b13e2adb185
Ilya Usov <iusov41@gmail.com>
2022-03-02 20:21:25 +0300

    working eslint+prettier+husky+lintstaged pack

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-const a = 15;
-
-const str1 = '111';
-const str2 = '123';
-
-console.log(a, str1, str2);
-
-const f = (b) => b + 123;
-
 const arr1 = [1, 2, 3, 5, 6];
 
 const arr2 = arr1.map((e) => e + 1);
 
 const arr3 = arr1.map((e) => e + 2);
 
-console.log(f(312), arr2, arr3);
+function f2(a1, a2) {
+  return a1[0] + a2[0];
+}
 
-console.log('aaaaa');
+f2(arr2, arr3);
