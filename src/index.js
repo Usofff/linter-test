5f7e2859bceae866ab15da7361f2b22c009f847e
Ilya Usov <iusov41@gmail.com>
2022-03-02 20:03:15 +0300

    init eslint-config

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const a = 15;
+
+const str1 = '111';
+const str2 = '123';
+
+console.log(a, str1, str2);
+
+const f = (b) => b + 123;
+
+const arr1 = [1, 2, 3, 5, 6];
+
+const arr2 = arr1.map((e) => e + 1);
+
+const arr3 = arr1.map((e) => e + 2);
+
+console.log(f(312), arr2, arr3);
